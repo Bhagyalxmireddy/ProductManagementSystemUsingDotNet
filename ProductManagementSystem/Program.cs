9f38dca0a75d43e13b6e40ae9f280fefa36aacd3
Bhagi <puppyreddy1997@gmail.com>
2020-12-18 18:58:56 +0530

    [Bhagyalaxmi]ADDED:Printing the Welcome Message

---
+++ ProductManagementSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management system");
+        }
+    }
+}
